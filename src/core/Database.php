511b217831fef02fe8c7b7296f735a4ff9c68b53
BrunoPinheiro <87577359+BrunoPinheirofe@users.noreply.github.com>
2025-05-29 17:04:29 -0300

    fix: Correct PDO instantiation and method visibility in Database class

--- src/core/Database.php
+++ src/core/Database.php
@@ -5,19 +5,18 @@ class Database{
     private $db;
 
     public function __construct() {
-        $this->db = new PDO('mysql:host=localhost;dbname=seu_banco_de_dados', 'usuario', 'senha');
+        $this->db = new PDO("mysql:host=localhost;dbname=seu_banco_de_dados', 'usuario', 'senha");
         
     }
-    public static function query($query, $params = [], $class){
+    public function query($query, $params = [], $class){
         
         $prepare = $this->db->prepare($query);
         if(!$prepare){
             throw new Exception("Erro ao preparar a query: " . implode(", ", $this->db->errorInfo()));
         }
 
-
         if($class){
-            $this->db->setFetcMode(PDO::FETCH_CLASS, $class);
+            $prepare->setFetchMode(PDO::FETCH_CLASS, $class);
         }
         $prepare->execute($params);
         return $prepare;
