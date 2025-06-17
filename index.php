871f3b9f0c8cf3ee463bb9e52cd5ef78c7dbdc3c
jhnefer <jhenefer.fdc@gmail.com>
2025-06-17 19:18:51 -0300

    tentando fazer pull

--- index.php
+++ index.php
@@ -35,9 +35,11 @@ $router = new Router(URL);
 include_once(__DIR__ . "/src/routers/web.php");
 include_once(__DIR__ . "/src/routers/admin.php");
 include_once(__DIR__ . "/src/routers/auth.php");
+/*
 echo "<pre>";
 print_r($_SESSION['user']);
 echo "</pre>";
+*/
 
 
 
